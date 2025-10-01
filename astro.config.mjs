7f6a51343dbaa3c26e079c9c0d3bf0658be36647
Maria Chaparro <maferch2002@gmail.com>
2025-10-01 09:56:05 -0400

    Vercel deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import preact from '@astrojs/preact';
-import node from '@astrojs/node'
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   integrations: [preact()],
-  output: 'static',
-  adapter: node({
-    mode: 'standalone'
-  })
+
+  adapter: vercel(),
 });
